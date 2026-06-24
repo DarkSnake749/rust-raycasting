b1d738ad6030cf286d5382f431ea2f486c96d06e
DarkSnake749 <etienne.boule@hotmail.com>
2026-06-24 17:44:14 -0400

    App initialized with a basic map

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,56 @@
 use macroquad::prelude::*;
+mod palette;
 
-fn main() {
-    println!("Hello, macroquad!");
+const CELL_SIZE: f32 = 32.;
+const PLAYER_SIZE: f32 = 16.;
+
+struct Map {
+    data: Vec<u8>,
+    width: usize,
+    height: usize,
+}
+
+#[macroquad::main("Raycaster")]
+async fn main() {
+    request_new_screen_size(1080., 720.);
+
+    let map: Map = Map { 
+        data: vec![
+            1, 1, 1, 1, 1, 1, 1, 1, 1, 1,
+            1, 0, 0, 0, 0, 0, 0, 0, 1, 1,
+            1, 0, 1, 0, 0, 0, 0, 1, 1, 1,
+            1, 0, 0, 0, 0, 1, 0, 0, 0, 1,
+            1, 0, 0, 0, 0, 1, 0, 0, 0, 1,
+            1, 0, 0, 0, 0, 1, 0, 0, 0, 1,
+            1, 0, 0, 0, 0, 0, 0, 0, 0, 1,
+            1, 0, 0, 1, 1, 1, 0, 0, 0, 1,
+            1, 1, 0, 0, 0, 0, 0, 1, 1, 1,
+            1, 1, 1, 1, 1, 1, 1, 1, 1, 1,
+        ], 
+        width: 10, 
+        height: 10,
+    };
+
+    loop {
+        clear_background(palette::BLACK);
+        draw_map(&map);
+
+        next_frame().await
+    }
+}
+
+fn draw_map(map: &Map) {
+    for y in 0..map.height {
+    for x in 0..map.width  {
+        let idx = y * map.width + x;
+        let wall = map.data[idx] != 0;
+
+        if wall {
+            draw_rectangle(
+                (x as f32) *CELL_SIZE, (y as f32) *CELL_SIZE, 
+                CELL_SIZE, CELL_SIZE, 
+                palette::DARK_GREY
+            );
+        }
+    }}
 }
---
+++ src/palette.rs
@@ -0,0 +1,21 @@
+use macroquad::prelude::Color;
+
+/* 
+
+Color palette: gang gang cockatoo
+By: bacondgoat
+src: https://www.color-hex.com/color-palette/1080779
+
+#f44336
+#999999
+#5b5b5b
+#2b2929
+#cc0000
+
+*/
+
+pub const RED: Color        = Color::from_hex(0xf44336);
+pub const LIGHT_GREY: Color = Color::from_hex(0x999999);
+pub const DARK_GREY: Color  = Color::from_hex(0x5b5b5b);
+pub const BLACK: Color      = Color::from_hex(0x2b2929);
+pub const DARK_RED: Color   = Color::from_hex(0xcc0000);
